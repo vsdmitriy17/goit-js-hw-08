a0012d3ab4209ffcfb81beb5c5d4cf691e8c8548
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-01-17 18:01:51 +0200

    Task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,65 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+//Находим элемент div.gallery
+const galleryEl = document.querySelector('.gallery');
+//Генерируем строку с разметкой карточек галереи изображений
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+//Добавляем разметку карточек галереи изображений в элемент galleryEl
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+//Добавляем слушателя события 'click' на элемент galleryEl
+galleryEl.addEventListener('click', onGalleryCardClick);
+
+// Ф-ция: принимает массив объектов (galleryItems), с данными для карточек изображений, 
+//        возвращает строку с разметкой карточек галереи изображений
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`;
+    }).join('');
+}; 
+
+//Ф-ция:
+//     отменяет действия браузера по умолчанию;
+//     проверяет условие клика по элементу img (не реагирует на клик на др элементы);
+//     открывает слайдер (lightbox)    
+function onGalleryCardClick(evt) {
+    evt.preventDefault();
+
+    if(!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    openLightbox();
+};
+
+//Ф-ция - создает и открывает слайдер (lightbox - библиотека SimpleLightbox, класс SimpleLightbox, метод open()), с оригинальными (большими - original) изображениями;
+//     свойства - для описания изображений (подпись изображений):
+//             captionSelector - указывает элемент который содержит описание
+//             captionType - указывает где именно в элементе находится описание (атрибут, дата-атрибут, текст)
+//             captionsData - указывает в каком атрибуте хранится описание
+//             captionPosition - указывает положение описания (вверху, внизу, за пределами изображения)
+//             captionDelay - указывает задержку появления подписи
+//             enableKeyboard - позволяет навигацию с клавиатуры (<- ->) и выход при нажатии Esc
+function openLightbox() {
+    let lightbox = new SimpleLightbox('.gallery a',
+        {
+            captionSelector: 'img',
+            captionType: 'attr',
+            captionsData: 'alt',
+            captionPosition: 'bottom',
+            captionDelay: 250,
+            enableKeyboard: true,
+        });
+    lightbox.open();
+};
 
 console.log(galleryItems);
