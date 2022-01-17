664f4cc9d220c9cf9806c4f6b1501dd6b5588253
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-01-17 21:55:56 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+// Подключение VimeoPlayer и Lodash.trottle
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// Находим элемент iframe
+const iframe = document.querySelector('iframe');
+// Создаем элемент player, библиотеки Player
+const player = new Player(iframe);
+
+//Метод "player.on('event',callBackFunction)" (на элементе player - прослушивает событие 'event', вызывает callBackFunction),
+// при событии "timeupdate", вызывает колбек ф - цию "playerOn", через метод "Lodash.trottle"(вызов ф - ции с периодичностью 1000 мс)
+player.on('timeupdate', throttle(playerOn, 1000));
+
+// Метод player.setCurrentTime(seconds) - устанавливает время старта видео, при запуске плеера
+// аргумент seconds - время в секудах (localStorage.getItem('videoplayer-current-time') - время из локального хранилища)
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+//Ф-ция - принимает объект события "timeupdate" ({duration: 61.857, percent: 0.049, seconds: 3.034}),
+//Сохраняет в локальное хранилище текущее время видео (time), ключ - 'videoplayer-current-time'
+function playerOn(data) {
+    const time = data.seconds;
+    localStorage.setItem('videoplayer-current-time', time);
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+// Подключение VimeoPlayer и Lodash.trottle
+import throttle from 'lodash.throttle';
 
+//Находим элементы form, input, textarea
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
+
+//Слушатели событий
+formEl.addEventListener('submit', onFormSubmit);
+inputEl.addEventListener('input', throttle(onInputChange, 500));
+textareaEl.addEventListener('input',throttle(onTextareaChange, 500));
+
+function onInputChange(evt) {
+    const value = evt.currentTarget.value;
+};
+
+function onTextareaChange(evt) {
+    
+};
+
+function onFormSubmit(evt) {
+    
+};
