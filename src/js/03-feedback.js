faf814ad6a1a8cb04a932141b57c5461870ae5e8
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-01-19 22:18:29 +0200

    task-3/bag-fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 // Подключение Lodash.trottle
 import throttle from 'lodash.throttle';
 
-//Находим элементы form, input, textarea
+//Находим элементы form, input, textarea, button
 const formEl = document.querySelector('.feedback-form');
 const inputEl = document.querySelector('.feedback-form input');
 const textareaEl = document.querySelector('.feedback-form textarea');
@@ -23,6 +23,7 @@ textareaEl.addEventListener('input', throttle(onTextareaChange, 500));
 //Ф-ция:
 //  1) присваивает свойству email, объекта formState - текущее значение поля формы input
 //  2) сохраняет в локальное хранилище текущий объект formState (JSON - строку)
+//  3) включает, либо выключает кнопку
 function onInputChange(evt) {
     formState.email = evt.currentTarget.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
@@ -32,6 +33,7 @@ function onInputChange(evt) {
 //Ф-ция:
 //  1) присваивает свойству message, объекта formState - текущее значение поля формы textarea
 //  2) сохраняет в локальное хранилище текущий объект formState (JSON - строку)
+//  3) включает, либо выключает кнопку
 function onTextareaChange(evt) {
     formState.message = evt.currentTarget.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
@@ -43,11 +45,15 @@ function onTextareaChange(evt) {
 //   2) выводит в консоль объект, созданный из текущих данных в локальном хронилище по ключу STORAGE_KEY
 //   3) очищает все поля формы
 //   4) удаляет из локального хранилища данные по ключу STORAGE_KEY
+//   5) обнуляет значения свойств объекта formState
+//   6) отключает кнопку
 function onFormSubmit(evt) {
     evt.preventDefault();
     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    formState.email = "";
+    formState.message = "";
     btnDisable();
 };
 
@@ -56,13 +62,16 @@ function onFormSubmit(evt) {
 //   2) проверяет условие наявности данных в локальном хранилище (т.е. feedback приводится к true), в случае выполнения условия:
 //       - присваивает полю input формы - значение свойства email объекта feedback
 //       - присваивает полю textarea формы - значение свойства message объекта feedback
-//       т.е. заполняет поля формы значениями из локального хранилища
+//         т.е. заполняет поля формы значениями из локального хранилища
+//       -присваивает соотв. значения ключам объекта formState
 function populateFormState() { 
     const feedback = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
     if (feedback) { 
         inputEl.value = feedback.email;
         textareaEl.value = feedback.message;
+        formState.email = feedback.email;
+        formState.message = feedback.message;
     };
 };
 
